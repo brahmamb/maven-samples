ca1974312543b7e35560f14de362ec384def2bbf
Poonam Agarwal <agrawal.poonam2015@gmail.com>
2021-09-29 21:27:42 +0530

    applying ignore annotation

--- single-module/src/test/java/com/example/TestGreeter.java
+++ single-module/src/test/java/com/example/TestGreeter.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.Ignore;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
